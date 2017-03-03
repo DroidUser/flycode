62a8a5e0236a45fb15514c6a3a18307ff6c5dba4
DroidUser <kumar.kailash38@gmail.com>
2017-03-03 16:28:19 +0530

    Update EmptyNodeSetup.sh

--- scripts/EmptyNodeSetup.sh
+++ scripts/EmptyNodeSetup.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 echo "Empty node setup"
+TMPFOLDER=/tmp/hue
+
+rm -rf $TMPFOLDER
+mkdir -p $TMPFOLDER
+echo "Downloading Hue tar file"
+wget https://s3.amazonaws.com/infoworks-setup/tmp/infoworks-testing.tar.gz -P $TMPFOLDER
 #sudo echo "Empty node setup" > /opt/welcome-su.txt
 #sudo apt-get install subversion
