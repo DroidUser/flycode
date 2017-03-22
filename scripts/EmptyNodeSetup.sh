5b1d6c788210eb244b44965f83bd7eedcf008d26
DroidUser <kumar.kailash38@gmail.com>
2017-03-22 15:51:27 +0530

    Update EmptyNodeSetup.sh

--- scripts/EmptyNodeSetup.sh
+++ scripts/EmptyNodeSetup.sh
@@ -8,66 +8,70 @@ export configured_status_file=configured
 export username=infoworks-user
 export password=welcome
 
+export k1=$1
+export k2=$2
+export k3=$3
+
 #create system user with sudo permission
 _create_user(){
 
-	echo "[$(date +"%m-%d-%Y %T")] Creating user $username"
-	{
-		#check whether the cmd is run by root
-		if [ $(whoami) = "root" ]; then
-			egrep "^$username" /etc/passwd >/dev/null
-			if [ $? -eq 0 ]; then
-				echo "$username exists!"
-			else
-				pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-				useradd -m -p $pass $username
-				[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
-			fi
-			usermod -aG sudo $username || echo "Could not give sudo permission to $username"
-			
-		else
-			echo "Only root may add a user to the system"
-			return 1
-		fi
+    echo "[$(date +"%m-%d-%Y %T")] Creating user $username"
+    {
+        #check whether the cmd is run by root
+        if [ $(whoami) = "root" ]; then
+            egrep "^$username" /etc/passwd >/dev/null
+            if [ $? -eq 0 ]; then
+                echo "$username exists!"
+            else
+                pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+                useradd -m -p $pass $username
+                [ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+            fi
+            usermod -aG sudo $username || echo "Could not give sudo permission to $username"
+            
+        else
+            echo "Only root may add a user to the system"
+            return 1
+        fi
 
-	} || {
-		echo 'Could not add user $username' && return 1
-	}
+    } || {
+        echo 'Could not add user $username' && return 1
+    }
 }
 
 _extract_file(){
 
-	echo "Extracting infoworks package $1"
-	if [ -f $1 ] ; then
-	 case $1 in
-	     *.tar.gz)    tar -xzf $1 ;;
-	     *.zip)       unzip $1 -d ${app_name} ;;
-	     *)           echo "'$1' cannot be extracted" ;;
-	 esac
-	else
-	 echo "'$1' is not a valid file"
-	fi
+    echo "Extracting infoworks package $1"
+    if [ -f $1 ] ; then
+     case $1 in
+         *.tar.gz)    tar -xzf $1 ;;
+         *.zip)       unzip $1 -d ${app_name} ;;
+         *)           echo "'$1' cannot be extracted" ;;
+     esac
+    else
+     echo "'$1' is not a valid file"
+    fi
 }
 
 
 #download infoworks package
 _download_app(){
 
-	echo "[$(date +"%m-%d-%Y %T")] Started downloading application from "${app_path}
-	{
-		eval cd /opt/ && wget ${app_path} && {
-			for i in `ls -a`; do
-				if [[ ($app_path =~ .*$i.*) && -f $i ]]; then
-					_extract_file $i;
-				fi
-			done
-		} || return 1;
+    echo "[$(date +"%m-%d-%Y %T")] Started downloading application from "${app_path}
+    {
+        eval cd /opt/ && wget ${app_path} && {
+            for i in `ls -a`; do
+                if [[ ($app_path =~ .*$i.*) && -f $i ]]; then
+                    _extract_file $i;
+                fi
+            done
+        } || return 1;
 
-		eval chown -R $username:$username ${app_name} || echo "Could not change ownership of infoworks package"
+        eval chown -R $username:$username ${app_name} || echo "Could not change ownership of infoworks package"
 
-	} || {
-		echo "Could not download the package" && return 1
-	}
+    } || {
+        echo "Could not download the package" && return 1
+    }
 }
 
 #find active namenode of the cluster
@@ -104,61 +108,72 @@ export -f _get_namenode_hostname
 
 _deploy_app(){
 
-	echo "[$(date +"%m-%d-%Y %T")] Started deployment"
+    echo "[$(date +"%m-%d-%Y %T")] Started deployment"
     _get_namenode_hostname namenode_hostname `hostname -f`
-	hiveserver_hostname="hive2://$namenode_hostname:10000"
-	sparkmaster_hostname="spark://$namenode_hostname:7077"
+    hiveserver_hostname="hive2://$namenode_hostname:10000"
+    sparkmaster_hostname="spark://$namenode_hostname:7077"
 
-	#input parameters prompted by start.sh
-	expect <<-EOF
-		spawn su -c "$iw_home/bin/start.sh all" -s /bin/sh $username
+    #input parameters prompted by start.sh
+    expect <<-EOF
+        spawn su -c "$iw_home/bin/start.sh all" -s /bin/sh $username
 
-		expect "HDP installed"
-		sleep 1
-		send "\n"
+        expect "HDP installed"
+        sleep 1
+        send "\n"
 
-		expect "namenode"
-	        sleep 1
-		send $namenode_hostname\n
+        expect "namenode"
+            sleep 1
+        send $namenode_hostname\n
 
-		expect "Enter the path for infoworks hdfs home"
-	        sleep 1
-		send "\n"
+        expect "Enter the path for infoworks hdfs home"
+            sleep 1
+        send "\n"
 
-		expect "HiveServer2"
-	        sleep 1
-		send $hiveserver_hostname\n
+        expect "HiveServer2"
+            sleep 1
+        send $hiveserver_hostname\n
 
-		expect "username"
-	        sleep 1
-		send "\n"
+        expect "username"
+            sleep 1
+        send "\n"
 
-		expect "password"
-	        sleep 1
-		send "\n"
+        expect "password"
+            sleep 1
+        send "\n"
 
-		expect "hive schema"
-	        sleep 1
-		send "\n"
+        expect "hive schema"
+            sleep 1
+        send "\n"
 
-		expect "Spark master"
-	        sleep 1
-		send $sparkmaster_hostname\n
+        expect "Spark master"
+            sleep 1
+        send $sparkmaster_hostname\n
 
-		expect "Infoworks UI"
-	        sleep 1
-		send "\n"
+        expect "Infoworks UI"
+            sleep 1
+        send "\n"
 
-		interact
-	EOF
+        interact
+EOF
 
-	if [ "$?" != "0" ]; then
-		return 1;
-	fi
+    if [ "$?" != "0" ]; then
+        return 1;
+    fi
+}
+
+_config_hdi_params() {
+    k1=$(source /opt/infoworks/bin/env.sh; /opt/infoworks/apricot-meteor/infoworks_python/infoworks/bin/infoworks_security.sh -encrypt -p "$k1")
+    k2=$(source /opt/infoworks/bin/env.sh; /opt/infoworks/apricot-meteor/infoworks_python/infoworks/bin/infoworks_security.sh -encrypt -p "$k2")
+    k3=$(source /opt/infoworks/bin/env.sh; /opt/infoworks/apricot-meteor/infoworks_python/infoworks/bin/infoworks_security.sh -encrypt -p "$k3")
+    echo  "#iw cdw properties" >> /opt/infoworks/conf/conf.properties
+    echo  "iw_cdw_k1=$k1" >> /opt/infoworks/conf/conf.properties
+    echo  "iw_cdw_k2=$k2" >> /opt/infoworks/conf/conf.properties
+    echo  "iw_cdw_k3=$k3" >> /opt/infoworks/conf/conf.properties
+    
 }
 
 #install expect tool for interactive mode to input paramenters
 apt-get --assume-yes install expect
 [ $? != "0" ] && echo "Could not install 'expect' plugin" && exit 
 
-eval _create_user && _download_app && _deploy_app && [ -f $configured_status_file ] && echo "Application deployed successfully"  || echo "Deployment failed"
+eval _create_user && _download_app && _deploy_app && [ -f $configured_status_file ] && _config_hdi_params && echo "Application deployed successfully"  || echo "Deployment failed"
