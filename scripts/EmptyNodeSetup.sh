67c95a7e2f3c3c6d02d84e7915463c2e0b92a831
DroidUser <kumar.kailash38@gmail.com>
2017-04-07 18:56:28 +0530

    Update EmptyNodeSetup.sh

--- scripts/EmptyNodeSetup.sh
+++ scripts/EmptyNodeSetup.sh
@@ -11,7 +11,7 @@ export k2=$2
 export k3=$3
 export k4=$4
 export username=infoworks-user
-export password=$k5
+export password=$5
 
 printf "got parameters k1=%s k2=%s k3=%s k4=%s password=%s " "$k1" "$k2" "$k3" "$k4" "$password"
 
