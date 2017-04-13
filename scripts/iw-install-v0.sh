0eb95e54c9ad793f07dfafb0e5ce0f8a49184a58
DroidUser <kumar.kailash38@gmail.com>
2017-04-13 14:19:49 +0530

    Add files via upload

---
+++ scripts/iw-install-v0.sh
@@ -0,0 +1,11 @@
+export k1=$1
+export k2=$2
+export k3=$3
+export k4=$4
+export username=infoworks-user
+export password=$5
+export saskey=$6
+
+printf "got parameters k1=%s k2=%s k3=%s k4=%s password=%s url=%s" "$k1" "$k2" "$k3" "$k4" "$password" "$saskey"
+
+wget saskey'.sh'
