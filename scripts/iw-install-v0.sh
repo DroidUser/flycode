45e80ad45e073fc4c7aa51f3b0e683675f0022ab
DroidUser <kumar.kailash38@gmail.com>
2017-04-13 17:51:24 +0530

    Update iw-install-v0.sh

--- scripts/iw-install-v0.sh
+++ scripts/iw-install-v0.sh
@@ -8,17 +8,21 @@ export p4=$4
 export p5=$5
 export p6=$6
 
+_timestamp(){
+	date +%H:%M:%S
+}
+
 _download_file()
 {
     srcurl=$1;
     destfile=$2;
-    overwrite=$3;
 
     if [ -e $destfile ]; then
         return;
     fi
     echo "[$(_timestamp)]: downloading $1"
     wget -O $destfile -q $srcurl;
+    echo "[$(_timestamp)]: downloaded $1 successfully"
 }
 
 _init(){
@@ -29,7 +33,9 @@ _init(){
 	#extract key
 	value=$(<secret.txt)
 	rm -rf secret.txt
-
+	
+	echo "[$(_timestamp)]: secret=$value"
+	
 	#download script file using key
 	_download_file https://iwteststorage.blob.core.windows.net/action-scripts-infoworks/sub_script.sh?st=2017-04-13T08%3A30%3A00Z&se=2020-04-13T08%3A30%3A00Z&sp=rl&sv=2015-12-11&sr=c&sig=${value} sub_script.sh
 
