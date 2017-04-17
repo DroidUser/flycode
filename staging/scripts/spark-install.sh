28b4a847af7887b7e14f46bb1108a0525bcd4fd7
DroidUser <kumar.kailash38@gmail.com>
2017-04-17 19:08:45 +0530

    Update spark-install.sh

--- staging/scripts/spark-install.sh
+++ staging/scripts/spark-install.sh
@@ -123,7 +123,7 @@ _init(){
 	cp -r livy/ /usr/hdp/$HDP_VERSION/
 	
 	#download the spark config tar file
-	_download_file https://raw.githubusercontent.com/DroidUser/flycode/master/staging/sparkconf.tar.gz /sparkconf.tar.gz
+	_download_file https://iwteststorage.blob.core.windows.net/utility-infoworks/sparkconf.tar.gz /sparkconf.tar.gz
 	
 	# Untar the Spark config tar.
 	mkdir /spark-config
